136229ee03f2d8a84eb3fa0218ebd4bf41735401
unknown <chesika968@gmail.com>
2023-03-28 19:58:57 +0200

    rollback to working condition before 3 new functions

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
@@ -5,3 +8,15 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def __init__(self, browser, url, timeout=4):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, CSS_SELECTOR, login_link):
+        try:
+            self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        except (NoSuchElementException):
+            return False
+        return True
