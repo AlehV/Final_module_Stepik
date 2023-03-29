b257a6b9f4f5d53680c6e7fc096309ded3210a97
unknown <chesika968@gmail.com>
2023-03-29 10:07:13 +0200

    Fixed issues for 3 new features

--- pages/base_page.py
+++ pages/base_page.py
@@ -14,9 +14,9 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def is_element_present(self, CSS_SELECTOR, login_link):
+    def is_element_present(self, how, what):
         try:
-            self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+            self.browser.find_element(how, what)
         except (NoSuchElementException):
             return False
         return True
