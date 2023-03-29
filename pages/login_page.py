7585624f26dab9c45f8b3b17631732cf7c14dc7d
unknown <chesika968@gmail.com>
2023-03-29 09:15:09 +0200

    Working Verify login link function

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "This is not a login URL"
+
+      
+    def should_be_login_form(self):
+        #assert self.is_element_present(By.CSS_SELECTOR, "#login_form"), "Login form is not presented"
+        self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert True
+       #   в shold_be_login_form ищем через id login_form в locators
+
+
+    def should_be_register_form(self):
+        assert  self.is_element_present(By.CSS_SELECTOR, "#register_form") == True, "Register form is not presented"
